b619052890c16e0556ad66d57ef68c223e1f2591
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2025-10-17 08:31:47 +0700

    reset

--- src/components/Input.jsx
+++
@@ -1,9 +0,0 @@
-export default function Input({ label, error, className = "", ...props }) {
-  return (
-    <label className={`input-block ${className}`}>
-      {label && <span className="label">{label}</span>}
-      <input className="input" {...props} />
-      {error && <span className="error">{error}</span>}
-    </label>
-  )
-}
